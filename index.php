9cb86412312e0366af5fedaedff6c8099959bf57
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-12-13 22:59:31 +0100

    layout da finire

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include __DIR__ .'/partials/head.php';
+include __DIR__ . '/partials/head.php';
 
 
 
@@ -9,8 +9,8 @@ include_once __DIR__ . '/Models/Beds.php';
 
 
 
-$dog = new Category('cane', 'dog.png');
-$cat = new Category('gatto', 'cat.png');
+$dog = new Category('dogs', 'dog.png');
+$cat = new Category('cats', 'cat.png');
 
 
 
@@ -18,11 +18,13 @@ $cat = new Category('gatto', 'cat.png');
 
 
 
-$food = new Food('ciccioli', 'crr.jpj', 9.8, $dog, -3, ['carne', 'patate', 'banane'], '2022-12-11');
+$food = new Food('ciccioli', 'dogfood1.jpg', 34.89, $dog, 45, ['manzo', 'patate', 'maiale', 'pollo'], '2022-12-11');
+$food1 = new Food('Miaonese', 'catfood.jpg', 12.99, $cat, 0.5, ['pesce', 'patate', 'riso', 'sale'], '2023-12-11');
+$food2 = new Food('Crocche', 'dogfood2.png', 43.89, $dog, 20, ['carote', 'patate', 'riso', 'sale', 'broccoli'], '2023-10-21');
 
 
 
-$game = new Games('osso', 'osso.jps', 12.30, $cat, 'small', ['stoffa', 'plastica']);
+$game = new Games('osso di corda', 'corda.jpg', 12.30, $dog, 'small', ['stoffa', 'plastica']);
 
 
 
@@ -38,74 +40,175 @@ $bed = new Beds('lettino', 'letto.jpg', 25.90, $dog, 'esterno', ['seta', 'cotone
 <body>
 
 
-<!-- header -->
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+    <!-- header -->
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Robe da Cani</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
 
 
 
-<!-- main -->
-    <div class="container">
+    <!-- main -->
+
+    <!-- cibo -->
+    <div class="container wrapper">
+        <h1>Food</h1>
         <div class="row">
             <div class="">
-            <div class="card-group">
-  <div class="card">
-    <img src="./img/<?php echo $food->category->getIcon()   ?>" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title"> <?php echo $food->getTitle()  ?></h5>
-      <p class="card-text"><?php  echo $food->getPrice() . '€' ?></p>
-      <p class="card-text"><small class="text-muted"><?php foreach( ($food->getIngredients())as $ingredients) echo $ingredients . ' ' ?></small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-</div>
+                <div class="card-group">
+                    <div class="card">
+                        <div class="text-center p-3">
+
+                            <img src="./img/<?php echo $food->getImage() ?>" class="card-img-top prodotto" alt="...">
+
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <h5 class="card-title col-6 fw-bold text-uppercase"> <?php echo $food->getTitle() ?></h5>
+                                <div class="col-6 d-flex align-content-center align-items-center justify-content-end">
+                                    <p class="m-0 me-2"><?php echo $food->category->getSpecies()  ?></p>
+                                    <img src="./img/<?php echo $food->category->getIcon() ?>" class="icona" alt="...">
+                                </div>
+                            </div>
+
+                            <p class="card-text fw-bold"><?php echo $food->getPrice() . '€' ?></p>
+                            <p class="card-text"><span>Ingredients: <br></span><small class="text-muted"><?php foreach (($food->getIngredients()) as $ingredients) echo $ingredients . ' ' ?></small></p>
+                            <p class="card-text small"> <span>Data di scadenza: <br></span> <?php echo $food->getExp_date()  ?></p>
+                            <button type="button" class="btn btn-primary">Add to chart</button>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="text-center p-3">
+                            <img src="./img/<?php echo $food1->getImage() ?>" class="card-img-top prodotto" alt="...">
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <h5 class="card-title col-6 fw-bold text-uppercase"> <?php echo $food1->getTitle() ?></h5>
+                                <div class="col-6 d-flex align-content-center align-items-center justify-content-end">
+                                    <p class="m-0 me-2"><?php echo $food1->category->getSpecies()  ?></p>
+                                    <img src="./img/<?php echo $food1->category->getIcon() ?>" class="icona" alt="...">
+                                </div>
+                            </div>
+                            <p class="card-text fw-bold"><?php echo $food1->getPrice() . '€' ?></p>
+                            <p class="card-text"><span>Ingredients: <br></span><small class="text-muted"><?php foreach (($food1->getIngredients()) as $ingredients) echo $ingredients . ' ' ?></small></p>
+                            <p class="card-text small"> <span>Data di scadenza: <br></span> <?php echo $food1->getExp_date()  ?></p>
+                            <button type="button" class="btn btn-primary">Add to chart</button>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="text-center p-3">
+                            <img src="./img/<?php echo $food2->getImage() ?>" class="card-img-top prodotto" alt="...">
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <h5 class="card-title col-6 fw-bold text-uppercase"> <?php echo $food2->getTitle() ?></h5>
+                                <div class="col-6 d-flex align-content-center align-items-center justify-content-end">
+                                    <p class="m-0 me-2"><?php echo $food2->category->getSpecies()  ?></p>
+                                    <img src="./img/<?php echo $food2->category->getIcon() ?>" class="icona" alt="...">
+                                </div>
+                            </div>
+                            <p class="card-text fw-bold"><?php echo $food2->getPrice() . '€' ?></p>
+                            <p class="card-text"><span>Ingredients: <br></span><small class="text-muted"><?php foreach (($food2->getIngredients()) as $ingredients) echo $ingredients . ' ' ?></small></p>
+                            <p class="card-text small"> <span>Data di scadenza: <br></span> <?php echo $food2->getExp_date()  ?></p>
+                            <button type="button" class="btn btn-primary">Add to chart</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
     </div>
 
+    <!-- Games -->
+    <div class="container wrapper">
+        <h1>Games</h1>
+        <div class="row">
+            <div class="">
+                <div class="card-group">
+                    <div class="card">
+                        <div class="text-center p-3">
+                            <img src="./img/<?php echo $game->getImage() ?>" class="card-img-top prodotto" alt="...">
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <h5 class="card-title col-6 fw-bold text-uppercase"> <?php echo $game->getTitle() ?></h5>
+                                <div class="col-6 d-flex align-content-center align-items-center justify-content-end">
+                                    <p class="m-0 me-2"><?php echo $game->category->getSpecies()  ?></p>
+                                    <img src="./img/<?php echo $game->category->getIcon() ?>" class="icona" alt="...">
+                                </div>
+                            </div>
+                            <p class="card-text fw-bold"><?php echo $game->getPrice() . '€' ?></p>
+                            <p class="card-text"><span>Ingredients: <br></span><small class="text-muted"><?php foreach (($game->getMaterial()) as $materials) echo $materials . ' ' ?></small></p>
+                            <button type="button" class="btn btn-primary">Add to chart</button>
+                        </div>
+                    </div>
+                   
+                    <div class="card">
+                        <div class="text-center p-3">
+                            <img src="./img/<?php echo $game->getImage() ?>" class="card-img-top prodotto" alt="...">
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <h5 class="card-title col-6 fw-bold text-uppercase"> <?php echo $game->getTitle() ?></h5>
+                                <div class="col-6 d-flex align-content-center align-items-center justify-content-end">
+                                    <p class="m-0 me-2"><?php echo $game->category->getSpecies()  ?></p>
+                                    <img src="./img/<?php echo $game->category->getIcon() ?>" class="icona" alt="...">
+                                </div>
+                            </div>
+                            <p class="card-text fw-bold"><?php echo $game->getPrice() . '€' ?></p>
+                            <p class="card-text"><span>Ingredients: <br></span><small class="text-muted"><?php foreach (($game->getMaterial()) as $materials) echo $materials . ' ' ?></small></p>
+                            <button type="button" class="btn btn-primary">Add to chart</button>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="text-center p-3">
+                            <img src="./img/<?php echo $game->getImage() ?>" class="card-img-top prodotto" alt="...">
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <h5 class="card-title col-6 fw-bold text-uppercase"> <?php echo $game->getTitle() ?></h5>
+                                <div class="col-6 d-flex align-content-center align-items-center justify-content-end">
+                                    <p class="m-0 me-2"><?php echo $game->category->getSpecies()  ?></p>
+                                    <img src="./img/<?php echo $game->category->getIcon() ?>" class="icona" alt="...">
+                                </div>
+                            </div>
+                            <p class="card-text fw-bold"><?php echo $game->getPrice() . '€' ?></p>
+                            <p class="card-text"><span>Ingredients: <br></span><small class="text-muted"><?php foreach (($game->getMaterial()) as $materials) echo $materials . ' ' ?></small></p>
+                            <button type="button" class="btn btn-primary">Add to chart</button>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+    </div>
 
     <!-- footer -->
 
 
-    
+
 </body>
