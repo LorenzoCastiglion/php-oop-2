30c6abf4b1b2627eb449dce6c882c067be1d5102
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-12-13 18:55:11 +0100

    proto layout da terminare

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ include_once __DIR__ . '/Models/Beds.php';
 
 
 
-$dog = new Category('cane', 'dog.jpg');
-$cat = new Category('gatto', 'cat.jpg');
+$dog = new Category('cane', 'dog.png');
+$cat = new Category('gatto', 'cat.png');
 
 
 
@@ -36,16 +36,49 @@ $bed = new Beds('lettino', 'letto.jpg', 25.90, $dog, 'esterno', ['seta', 'cotone
 
 
 <body>
+
+
+<!-- header -->
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+
+<!-- main -->
     <div class="container">
         <div class="row">
             <div class="">
             <div class="card-group">
   <div class="card">
-    <img src="..." class="card-img-top" alt="...">
+    <img src="./img/<?php echo $food->category->getIcon()   ?>" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <h5 class="card-title"> <?php echo $food->getTitle()  ?></h5>
+      <p class="card-text"><?php  echo $food->getPrice() . '€' ?></p>
+      <p class="card-text"><small class="text-muted"><?php foreach( ($food->getIngredients())as $ingredients) echo $ingredients . ' ' ?></small></p>
     </div>
   </div>
   <div class="card">
@@ -69,6 +102,9 @@ $bed = new Beds('lettino', 'letto.jpg', 25.90, $dog, 'esterno', ['seta', 'cotone
         </div>
 
     </div>
+
+
+    <!-- footer -->
 
 
     
--- partials/head.php
+++ partials/head.php
@@ -7,6 +7,7 @@
 <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossorigin='anonymous'>
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css' integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==' crossorigin='anonymous' referrerpolicy='no-referrer' />
 <link rel='stylesheet' href='./CSS/style.css'>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <script src='https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js'></script>
 <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
 <script src='./JS/script.js' defer></script>
@@ -14,3 +15,5 @@
 </head>
 <body>
 </body>
+
+
